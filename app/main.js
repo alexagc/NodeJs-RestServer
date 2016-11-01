b76015ca432d7193dc9e9d32dcf5d3dee7e5fbb4
alexagc <alexcanal@gmail.com>
2016-11-01 19:02:36 +0100

    Modify connection to mongo, now it return a promise

---
+++ app/main.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const app = express();
+const router = new express.Router();
+
+// Controllers
+const ItemsCtrl = require('./controllers/itemController');
+
+function connectDatabase(mongoDatabase) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(mongoDatabase, (err, res) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+function startServer() {
+  // Basic configuration
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  // Use this to log all request
+  app.use(morgan('combined'));
+
+  // The reques always past for this code
+  router.get('/', (req, res) => {
+    // In this part of code the rest api needs to return all available resources
+    // res.send(JSON.stringify(router.stack));
+  });
+
+  // TODO all the routes need to be in separate files outside the server launch
+  router.route('/items')
+    .get(ItemsCtrl.findAllItems)
+    .post(ItemsCtrl.addItem);
+
+  app.use('/api', router);
+
+  app.listen(3000, () => {
+    console.log('Node server running on http://localhost:3000');
+  });
+}
+
+module.exports = { startServer, connectDatabase };
--- server.js
+++ server.js
@@ -1,39 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const server = require('./app/main');
 
-const app = express();
-const router = new express.Router();
+const mongoDatabase = 'mongodb://localhost/test';
 
-// Controllers
-const ItemsCtrl = require('./app/controllers/itemController');
-
-// Basic configuration
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// Use this to log all request
-app.use(morgan('combined'));
-
-// The reques always past for this code
-router.get('/', (req, res) => {
-  // In this part of code the rest api needs to return all available resources
-  // res.send(JSON.stringify(router.stack));
-});
-
-// TODO all the routes need to be in separate files outside the server launch
-router.route('/items')
-  .get(ItemsCtrl.findAllItems)
-  .post(ItemsCtrl.addItem);
-
-app.use('/api', router);
-
-// TODO the connection to the mongoDb needs to be configurable
-mongoose.connect('mongodb://localhost/test', (err, res) => {
-  if (err) {
+server.connectDatabase(mongoDatabase)
+  .then(() => {
+    console.log('Connection succesfull to database');
+    server.startServer();
+  })
+  .catch((err) => {
     console.log(`ERROR: connecting to Database. ${err}`);
-  }
-  app.listen(3000, () => {
-    console.log('Node server running on http://localhost:3000');
   });
-});
