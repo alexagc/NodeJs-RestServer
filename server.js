6143a224f10683d42758b40047be4fae7eceec88
Alejandro Gomez Canal <acanal@localhost.localdomain>
2016-11-02 08:49:58 +0100

    Add reconnect function to database

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const server = require('./app/main');
 
 const mongoDatabase = 'mongodb://localhost/test';
 
-server.connectDatabase(mongoDatabase)
-  .then(() => {
-    console.log('Connection succesfull to database');
-    server.startServer();
-  })
-  .catch((err) => {
-    console.log(`ERROR: connecting to Database. ${err}`);
-  });
+function start() {
+  server.connectDatabase(mongoDatabase)
+    .then(() => {
+      console.log('Connection succesfull to database');
+      server.startServer();
+    })
+    .catch((err) => {
+      console.log(`ERROR: connecting to Database. ${err}`);
+      console.log('Trying to connect in 5 seconds');
+      setTimeout(() => {
+        start();
+      }, 5000);
+
+    });
+}
+
+start();
